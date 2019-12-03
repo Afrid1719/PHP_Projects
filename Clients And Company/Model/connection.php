45ff3385378daca35e79521fa3779e80c7769b27
Syed Afrid Ali <Afrid1719@users.noreply.github.com>
2019-12-03 23:22:36 +0530

    Connection to DB created
    connection class, info file and the structure has been created.

---
+++ Clients And Company/Model/connection.php
@@ -0,0 +1,23 @@
+<?php
+
+class Connect{
+	protected $con;
+	const hostName="localhost";
+	const userName="root";
+	const password="";
+	const dbName="Client_n_Company";
+
+	public function Connect(){
+		try{
+			$this->con=new mysqli(self::hostName,self::userName,self::password,self::dbName);
+			return $this->con;
+		}catch(Exception $e){
+			return $e->getMessage();
+		}
+	}
+}
+
+
+$ob=new Connect;
+var_dump($ob);
+?>
